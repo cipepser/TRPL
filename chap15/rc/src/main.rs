82a63948a93e401f50d8b1135c0d5b5ba2454c0d
cipepser <respepic@gmail.com>
2020-03-03 13:46:22 +0900

    add List with Rc

--- chap15/rc/src/main.rs
+++ chap15/rc/src/main.rs
@@ -1,3 +1,18 @@
+use std::rc::Rc;
+use List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
 }
