7aa6aeb9cbe564817026f7ff15d1a2c2a846f7e6
cipepser <respepic@gmail.com>
2020-03-03 13:48:31 +0900

    print strong_count

--- chap15/rc/src/main.rs
+++ chap15/rc/src/main.rs
@@ -9,10 +9,14 @@ enum List {
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("count after creating a = {}", Rc::strong_count(&a));
 
-    println!("{:?}", a);
-    println!("{:?}", b);
-    println!("{:?}", c);
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
